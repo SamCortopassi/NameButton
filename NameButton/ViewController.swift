7f1457fa2f70886138bfee98b133c44427c75562
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-12-26 16:03:46 -0600

    button

--- NameButton/ViewController.swift
+++ NameButton/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var itemTextField: UITextField!
+   
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+        print("add to-do item: \(itemTextField.text)")
+    }
+    
 }
 
